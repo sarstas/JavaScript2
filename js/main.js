7cd2f0d22fde618c8c8b5ed157fa464595f8275c
sarstas <sar.stas@mail.ru>
2021-04-23 14:39:16 +0300

    lesson-2 v1

--- js/main.js
+++ js/main.js
@@ -1,21 +1,42 @@
 'use strict';
-const products = [
-   {id: 1, title: 'Notebook', price: 20000},
-   {id: 2, title: 'Mouse', price: 1500},
-   {id: 3, title: 'Keyboard', price: 5000},
-   {id: 4, title: 'Gamepad', price: 4500},
-];
 
-const renderProduct = (title = 'Temporary plug', price = 'Temporary plug') => {
-   return `<div class="products__card">
-            <h3 class="products__title">${title}</h3>
-            <p class="products__price">price: <span class="products__price-col">${price}</span> rub</p>
-            <button class="products__btn by-btn">Add to card</button>
-         </div>`;
-};
+class GoodsItem {
+   constructor(title, price) {
+      this.title = title;
+      this.price = price;
+   }
+   render() {
+      return `<div class="products__card">
+               <h3>${this.title}</h3>
+               <p>${this.price}</p>
+               </div>`;
+   }
+}
 
-const renderProducts = (list) => {
-   document.querySelector('.products').innerHTML = list.map((item) => renderProduct(item.title, item.price)).join("");;
-};
+class GoodsList {
+   constructor() {
+      this.goods = [];
+   }
 
-renderProducts(products);
+   fetchGoods() {
+      this.goods = [
+         { title: 'Shirt', price: 150 },
+         { title: 'Socks', price: 50 },
+         { title: 'Jacket', price: 350 },
+         { title: 'Shoes', price: 250 },
+      ];
+   }
+   
+   render() {
+      let listHtml = '';
+      this.goods.forEach(good => {
+         const goodItem = new GoodsItem(good.title, good.price);
+         listHtml += goodItem.render();
+      });
+      document.querySelector('.products').innerHTML = listHtml;
+   }
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();

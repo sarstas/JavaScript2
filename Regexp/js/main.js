3f4ca5f1b55fd647dae11a5727b05ac36df92425
sarstas <sar.stas@mail.ru>
2021-05-08 17:57:52 +0300

    add Vue component

--- Regexp/js/main.js
+++ Regexp/js/main.js
@@ -4,12 +4,12 @@ const text =
          "One: 'Hi Mary.' Two: 'Oh, hi.' One: 'How are you doing?' Two: 'I'm doing alright.How about you ?' One: 'Not too bad. The weather is great isn't it ?' Two: 'Yes. It's absolutely beautiful today.' One: 'I wish it was like this more frequently.' Two: 'Me too.' One: 'So where are you going now?' Two: 'I'm going to meet a friend of mine at the department store' One: 'Going to do a little shopping?' Two: 'Yeah, I have to buy some presents for my parents.'  One: 'What's the occasion ?' Two: 'It's their anniversary.' One: 'That's great.Well, you better get going.You don 't want to be late.' Two: 'I'll see you next time.'  One: 'Sure.' Bye."
 
       // Задание №1
-      const regExp = /\'/g;
-      //console.log("Задание №1 !!!" + text.replace(regExp, '\"'));
+      const regExp = /'/g;
+      //console.log("Задание №1 !!!" + text.replace(regExp, '"'));
 
 
       //Задание №2
-      const regExp2 = /\B\'/g;
+      const regExp2 = /\B'|'\B\'/g;
       //console.log("Задание №2 !!!" + text.replace(regExp2, '\"'));
 
       // Задание №3
